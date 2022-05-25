764c29d03b18deb2df95c7fe5b469d78be5bdb6c
Mohammad Hamid <mohammad200h@hotmail.com>
2022-05-25 03:48:54 +0100

    added camera control and ws control

--- panda3dCollision/game.py
+++ panda3dCollision/game.py
@@ -4,11 +4,42 @@ from direct.showbase.ShowBase import ShowBase
 import random
 
 loadPrcFileData("", "load-file-type p3assimp")
+
+keyMap={
+    # translation
+    "up":False,
+    "down":False,
+    "left":False,
+    "right":False,
+    # rotation
+    "z_left":False,
+    "z_right":False,
+    "y_left":False,
+    "y_right":False
+
+}
+cameraMap={
+    "up":False,
+    "down":False,
+    "left":False,
+    "right":False,
+    "zoom_in":False,
+    "zoom_out":False
+}
+# callback function to update keyMap
+def updateKeyMap(key,state):
+    keyMap[key]=state
+    # print("keyMap:: ",keyMap)
+
+def updateCameraMap(key,state):
+    cameraMap[key]=state
+
 class Game(ShowBase):
 
     def __init__(self):
         super().__init__()
         self.counter =0
+        self.speed = 10
         # isolation parameters
         self.limit ={
             "up":1,
@@ -16,6 +47,8 @@ class Game(ShowBase):
         }
         self.move_down_flag=True
 
+        # keyboard event setup
+        self.keyboard_event_init()
         # camera setup
         self.setup_camera()
         # collison setup
@@ -28,13 +61,91 @@ class Game(ShowBase):
    
 
         # add task
-        taskMgr.add(self.update_ws, "UpdateWS")
+        taskMgr.add(self.update, "Update")
 
 
     def setup_camera(self):
         self.cam.setPos(0,-2,1)
         self.cam.lookAt(0,0,0)
+    
+    def keyboard_event_init(self):
+        # translation
+        self.accept("w",updateKeyMap,["up"   ,True])
+        self.accept("s",updateKeyMap,["down" ,True])
+        self.accept("a",updateKeyMap,["left" ,True])
+        self.accept("d",updateKeyMap,["right",True])
+        # rotation
+        self.accept("q",updateKeyMap,["z_left"  ,True])
+        self.accept("e",updateKeyMap,["z_right" ,True])
+        self.accept("z",updateKeyMap,["y_left"  ,True])
+        self.accept("x",updateKeyMap,["y_right" ,True])
+        # camera zoom 
+        self.accept("arrow_up",updateCameraMap,["up" ,True])
+        self.accept("arrow_down",updateCameraMap,["down" ,True])
+        self.accept("arrow_left",updateCameraMap,["left" ,True])
+        self.accept("arrow_right",updateCameraMap,["right" ,True])
+        self.accept("g",updateCameraMap,["zoom_out" ,True])
+        self.accept("h",updateCameraMap,["zoom_in" ,True])
 
+
+    def update_pos(self):
+        dt = globalClock.getDt()
+        # current position
+        pos = self.ws.getPos()
+        # current orientation
+        orn = self.ws.getHpr()
+
+        # update state
+        if keyMap["up"]:
+            pos.z += self.speed *dt  
+        if keyMap["down"]:
+            pos.z -= self.speed *dt
+
+        if keyMap["right"]:
+            pos.x += self.speed *dt       
+        if keyMap["left"]:
+            pos.x -= self.speed *dt
+        
+        if keyMap["z_right"]:
+            orn.x += 10*self.speed *dt       
+        if keyMap["z_left"]:
+            orn.x -= 10*self.speed *dt
+
+        if keyMap["y_right"]:
+            orn.z += 10*self.speed *dt       
+        if keyMap["y_left"]:
+            orn.z -= 10*self.speed *dt
+
+
+        self.ws.setPos(pos)
+        self.ws.setHpr(orn)
+
+        # restting keyMap
+        for key in keyMap.keys():
+            keyMap[key]=False
+
+        print("orn::",orn)
+    
+    def update_camera_zoom(self):
+        dt = globalClock.getDt()
+        pos = self.cam.getPos()
+        if cameraMap["up"]:
+            pos.z += self.speed *dt 
+        if cameraMap["down"]:
+            pos.z -= self.speed *dt 
+        if cameraMap["right"]:
+            pos.x += self.speed *dt 
+        if cameraMap["left"]:
+            pos.x -= self.speed *dt 
+        if cameraMap["zoom_out"]:
+            pos.y -= self.speed *dt 
+        if cameraMap["zoom_in"]:
+            pos.y += self.speed *dt 
+        
+        self.cam.setPos(pos)
+        for key in cameraMap.keys():
+            cameraMap[key]=False
+    
     def add_ws(self):
         self.ws = self.loader.loadModel("FF.egg")
         self.ws.setPos(0.03,0,0)
@@ -47,8 +158,7 @@ class Game(ShowBase):
         col.node().addSolid(CollisionBox(center, dx, dy, dz))
         col.show()
         base.cTrav.addCollider(col, self.notifier)
-        
-        
+          
 
     def add_target(self):
         self.cube = self.loader.loadModel("Cube.egg")
@@ -69,7 +179,6 @@ class Game(ShowBase):
         print("onCollision::entry:: ",entry)
    
 
-
     def setupCollision(self):
         base.cTrav = CollisionTraverser()
         base.cTrav.showCollisions(render)
@@ -77,8 +186,14 @@ class Game(ShowBase):
         self.notifier.addInPattern("%fn-in-%in")
         self.accept("ws-in-target", self.onCollision)
     
+    def update(self,task):
+        # self.update_ws()
+        self.update_pos()
+        self.update_camera_zoom()
 
-    def update_ws(self,task):
+        return task.cont
+
+    def update_ws(self):
         vel = self.ws.getPythonTag("velocity")
         if self.move_down_flag:
             self.moveDown(vel)
@@ -94,11 +209,7 @@ class Game(ShowBase):
             z=self.ws.getZ()
             if z >self.limit["down"]:
                self.move_down_flag = True 
-           
-   
-
-      
-        return task.cont
+         
 
     def moveDown(self,vel):
        
